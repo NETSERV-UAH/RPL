7408a928b45025267a05773171975cbd90a23045
Hedayat Hosseini <h.hosseini@aut.ac.ir>
2018-07-15 03:25:27 +0430

    Add files via upload

---
+++ RPL_IEEE802154_V1/src/routing/rpl/RPLRouting.h
@@ -0,0 +1,346 @@
+/***************************************************************************
+ * file:        RPLRouting.h
+ *
+ * author:      Hamidreza Kermajani
+ *
+ * copyright:   (C) 2013 UPC, Castelldefels, Spain.
+ *
+ * description: Implementation of the IPv6 Routing Protocol for Low power
+ *              and Lossy Networks (RPL).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ *
+ ***************************************************************************
+ * last modification: 09/29/2013
+ **************************************************************************/
+
+/*
+ * Copyright (C) 2018 Elisa Rojas(1), Hedayat Hosseini(2), Carles Gomez(3);
+ *                    (1) GIST, University of Alcala, Spain.
+ *                    (2) CEIT, Amirkabir University of Technology (Tehran Polytechnic), Iran.
+ *                    (3) UPC, Castelldefels, Spain.
+ *                    adapted for using on INET 3.6.3, and also included some changes such as using ICMPv6 messages
+ *                    for transmitting RPL control messages, interface table , and lifesycle modules
+*/
+
+//EXTRA BEGIN
+/*
+#ifndef RPL_h
+#define RPL_h
+
+#include <map>
+#include <omnetpp.h>
+
+
+#include "MiXiMDefs.h"
+#include "BaseNetwLayer.h"
+#include "SimpleAddress.h"
+#include "SimpleBattery.h"
+#include "DIOMsg_m.h"
+#include "DISMessage_m.h"
+
+
+class SimTracer;
+class DIOMsg;
+class DISMessage;
+*/
+
+#ifndef _RPL_SRC_ROUTING_RPLROUTING_H
+#define _RPL_SRC_ROUTING_RPLROUTING_H
+
+#include "src/networklayer/icmpv6/ICMPv6MessageRPL_m.h"
+#include "inet/common/INETDefs.h"
+#include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/networklayer/contract/ipv6/IPv6AddressType.h"
+#include "inet/networklayer/contract/IRoutingTable.h"
+#include "inet/common/lifecycle/ILifecycle.h"
+#include "inet/networklayer/contract/INetfilter.h"
+#include "inet/common/lifecycle/NodeStatus.h"
+
+#include "src/simulationManager/managerRPL.h"
+
+
+namespace rpl {
+using namespace inet;
+//EXTRA END
+
+struct DIOState{
+    int version;
+    int Sent;
+    int Received;
+    int Suppressed;
+};
+
+struct DISState{
+    int Sent;
+    int Received;
+    int Suppressed;
+};
+
+struct NodeState{
+    int Version;
+    int Collision;
+    int PacketLost;
+    struct DIOState DIO;
+    struct DISState DIS;
+
+    double *PowerConsumption;
+    int *Rank;
+    simtime_t *JoiningDODAGTime;
+    simtime_t DODAGsFormationTimeRecords;
+    //EXTRA BEGIN, variables for saving the number of table entries in each iteration
+    int numPreferedParents;
+    int numParents;
+    //EXTRA END
+    NodeState* Link;
+}*NodeStateHeader=NULL,*NodeStateLast=NULL,*NodeStateNew=NULL;
+
+
+
+/**
+ * @brief IPv6 Routing Protocol for LLNs (RPL)provides a mechanism whereby
+ * multipoint-to-point traffic from devices inside the LLN towards a central
+ * control point is supported.
+ *
+ *
+ * @ingroup netwLayer
+ * @author Hamidreza Kermajani
+ **/
+//EXTRA BEGIN
+//class RPL : public BaseNetwLayer
+class RPLRouting : public cSimpleModule, public ILifecycle//, public INetfilter::IHook//, public cListener
+
+//EXTRA END
+{
+public:
+    /** @brief Copy constructor is not allowed.
+     */
+   // RPLRouting(const RPLRouting&);
+    /** @brief Assignment operator is not allowed.
+     */
+   // RPLRouting& operator=(const RPLRouting&);
+
+
+    RPLRouting()
+        //: BaseNetwLayer() //EXTRA
+        : DIOheaderLength(0)
+        , DISheaderLength(0)
+        , macaddress()
+        , sinkAddress()
+        , debug(false)
+    {};
+
+    /** @brief Initialization of the module and some variables*/
+    virtual void initialize(int) override;  //virtual void initialize(int);  //EXTRA
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }  //EXTRA
+
+    void handleMessage(cMessage* msg) override;  //EXTRA
+
+    virtual void finish() override;  //virtual void finish();  //EXTRA
+    friend NodeState* CreateNewNodeState(int Index, int VersionNo, simtime_t Time, int NodeRank);
+    virtual ~RPLRouting();
+
+    enum messagesTypes {
+        UNKNOWN=0,
+        DATA,
+        Global_REPAIR_TIMER,
+        DIO,
+        SEND_DIO_TIMER,
+        DIS_UNICAST,
+        SEND_DIS_UNICAST_TIMER,
+        DIS_FLOOD,
+        SEND_DIS_FLOOD_TIMER,
+        RESET_Global_REPAIR_TIMER,
+    };
+
+    struct Route_Table
+    {
+        IPv6Address Address; //LAddress::L3Type Address;  //EXTRA
+        IPv6Address NextHop; //LAddress::L3Type NextHop;  //EXTRA
+        struct Route_Table* Link;
+    }*RoutingTable;
+    int NofEntry;
+
+    //EXTRA BEGIN
+    // environment
+    cModule *host = nullptr;
+    IRoutingTable *routingTable = nullptr;
+    IInterfaceTable *interfaceTable = nullptr;
+    managerRPL *pManagerRPL = nullptr;
+    INetfilter *networkProtocol = nullptr;
+    int interfaceID;
+    bool *hasRoute;
+
+    /** @brief Gate ids */
+    //@{
+    int icmpv6InGateId;
+    int icmpv6OutGateId;
+    //@}
+
+    // lifecycle
+    bool isOperational = false;
+
+    //EXTRA END
+
+    /**
+     * @brief Length of the NetwPkt header
+     * Read from omnetpp.ini
+     **/
+    int DIOheaderLength;
+    int DISheaderLength;
+    int headerLength;
+
+
+    MACAddress macaddress; // LAddress::L2Type macaddress;  //EXTRA
+
+    IPv6Address sinkAddress; //LAddress::L3Type sinkAddress;  //EXTRA
+    IPv6Address myNetwAddr;  //EXTRA
+
+    bool isSink; //EXTRA
+
+    bool useSimTracer;
+    bool trace, stats, debug;
+    double rssiThreshold;
+
+
+    cMessage* GRepairTimer;
+    cMessage* DIOTimer;
+    cMessage* DISTimer;
+
+    struct DIOStatus
+    {
+        unsigned int nbDIOSent;
+        unsigned int nbDIOReceived;
+        unsigned int nbDIOSuppressed;
+        int version;
+        struct DIOStatus* link;
+    }*DIOStatusHeader,*DIOStatusNew,*DIOStatusLast;
+
+    struct DISStatus
+    {
+        unsigned int nbDISSent;
+        unsigned int nbDISReceived;
+        unsigned int nbDISSuppressed;
+        struct DISStatus* link;
+    }*DISStatusHeader,*DISStatusNew,*DISStatusLast;
+
+
+    char *FilePath;
+    bool IsJoined;
+    bool DISEnable;
+
+    double GlobalRepairTimer;
+
+    double DIOIntMin;
+    int DIORedun;
+    int DIOIntDoubl;
+    simtime_t DIOIMaxLength;
+
+    double DISIntMin;
+    double DISStartDelay;
+    int DISRedun;
+    int DISIntDoubl;
+    simtime_t DISIMaxLength;
+    int DISVersion;
+
+
+
+    IPv6Address DODAGID; //LAddress::L3Type DODAGID;
+    int Rank;
+    simtime_t NodeStartTime;
+    int VersionNember;
+    int Grounded;
+    simtime_t TimetoSendDIO;
+    simtime_t TimetoSendDIS;
+    struct DODAGJoiningtime{
+        simtime_t TimetoJoinDODAG;
+        int version;
+        struct DODAGJoiningtime* link;
+    }*DODAGJoinTimeHeader,*DODAGJoinTimeLast,*DODAGJoinTimeNew;
+
+    int DIO_c;
+    simtime_t DIO_CurIntsizeNow,DIO_CurIntsizeNext;
+    simtime_t DIO_StofCurIntNow,DIO_StofCurIntNext;
+    simtime_t DIO_EndofCurIntNow,DIO_EndofCurIntNext;
+
+    int DIS_c;
+    simtime_t DIS_CurIntsizeNow,DIS_CurIntsizeNext;
+    simtime_t DIS_StofCurIntNow,DIS_StofCurIntNext;
+    simtime_t DIS_EndofCurIntNow,DIS_EndofCurIntNext;
+
+
+    IPv6Address PrParent; //LAddress::L3Type PrParent;  //EXTRA
+    int *NofParents;
+    int MaxNofParents;
+    struct ParentStructure{
+        IPv6Address ParentId; //LAddress::L3Type ParentId;  //EXTRA
+        int ParentRank;
+    }**Parents;
+    enum PARENT_TYPES
+    {
+      NOT_EXIST,
+      EXIST,
+      SHOULD_BE_UPDATED,
+    };
+
+    /** @brief Handle messages from upper layer */
+    virtual void handleUpperMsg(cMessage* msg);
+
+    //EXTRA BEGIN
+    /** @brief Handle messages from lower layer */
+    //virtual void handleLowerMsg(cMessage* msg);
+
+    /** @brief Handle messages from the ICMPv6 module */
+    virtual void handleIncommingMessage(cMessage* msg);
+    //EXTRA END
+
+    /** @brief Handle self messages */
+    virtual void handleSelfMsg(cMessage* msg);
+
+    /** @brief Handle control messages from lower layer */
+    virtual void handleLowerControl(cMessage* msg);
+
+
+    /** @brief Decapsulate a message */
+    cMessage* decapsMsg(ICMPv6DIOMsg *msg);
+
+    /** @brief scheduling next DIO message transmission. */
+    virtual void scheduleNextDIOTransmission();
+    void scheduleNextDISTransmission();
+    void ScheduleNextGlobalRepair();
+    void DeleteScheduledNextGlobalRepair();
+
+    void TrickleReset();
+    void DeleteDIOTimer();
+    void SetDIOParameters();
+    void SetDISParameters();
+
+    //EXTRA BEGIN
+    //virtual int  IsParent(const LAddress::L3Type& id,int idrank);
+    //virtual void AddParent(const LAddress::L3Type& id,int idrank);
+    //virtual void DeleteParent(const LAddress::L3Type& id);
+
+    virtual int  IsParent(const IPv6Address& id,int idrank);
+    virtual void AddParent(const IPv6Address& id,int idrank);
+    virtual void DeleteParent(const IPv6Address& id);
+
+    //EXTRA END
+
+    DIOStatus* CreateNewVersionDIO(void);
+    DISStatus* CreateNewVersionDIS(void);
+    void DISHandler();
+    DODAGJoiningtime* CreateNewVersionJoiningTime(void);
+
+    //EXTRA
+    virtual bool handleOperationStage(LifecycleOperation *operation, int stage, IDoneCallback *doneCallback) override;
+
+};
+
+} // namespace rpl
+
+#endif  // ifndef _RPL_SRC_ROUTING_RPLROUTING_H
+
---
+++ RPL_IEEE802154_V1/src/simulationManager/managerRPL.cc
@@ -0,0 +1,214 @@
+// Copyright (C) 05/2013 Elisa Rojas
+//      Implements the base class for UDPFlowGenerator and TCPFlowGenerator, which share functionality
+/*
+ * Copyright (C) 2018 Elisa Rojas(1), Hedayat Hosseini(2);
+ *                    (1) GIST, University of Alcala, Spain.
+ *                    (2) CEIT, Amirkabir University of Technology (Tehran Polytechnic), Iran.
+ *                    INET 3.6.3 adaptation, also adapted for using in the wARP-PATH protocol
+*/
+/*
+ * Copyright (C) 2018 Elisa Rojas(1), Hedayat Hosseini(2);
+ *                    (1) GIST, University of Alcala, Spain.
+ *                    (2) CEIT, Amirkabir University of Technology (Tehran Polytechnic), Iran.
+ *                    INET 3.6.3 adaptation, also adapted for using in the IoTorii(WSN) protocol
+*/
+/*
+ * Copyright (C) 2018 Elisa Rojas(1), Hedayat Hosseini(2), Carles Gomez(3);
+ *                    (1) GIST, University of Alcala, Spain.
+ *                    (2) CEIT, Amirkabir University of Technology (Tehran Polytechnic), Iran.
+ *                    (3) UPC, Castelldefels, Spain.
+ *                    INET 3.6.3 adaptation, also adapted for using in the RPL protocol as a simulation manager module
+*/
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include <algorithm>
+#include "src/simulationManager/managerRPL.h"
+
+#include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/networklayer/common/L3AddressResolver.h"
+#include "inet/networklayer/ipv6/IPv6InterfaceData.h"
+
+
+namespace rpl {
+using namespace inet;
+
+Define_Module(managerRPL);
+
+void managerRPL::initialize(int stage)
+{
+    EV << "->managerRPL::initialize()" << endl;
+    if(stage == INITSTAGE_NETWORK_LAYER_3) // for preparing IP addresses of interface table
+    {
+        extractTopology();
+    }
+    EV << "<-managerRPL::initialize()" << endl;
+}
+
+void managerRPL::extractTopology()
+{
+    EV << "->managerRPL::extractTopology()" << endl;
+
+    // extract topology
+    cTopology topo("topo");
+    topo.extractByProperty("networkNode"); // topo.extractByProperty("node");
+    //cModule *mod = topo.getNode(0)->getModule();
+
+    // fill in NodeInfoVector (isHost and names) and HostInfoVector (IP and MAC addresses)
+    nodeInfo.resize(topo.getNumNodes());
+    unsigned int nWSN = 0;
+
+    for (unsigned int i=0; i<topo.getNumNodes(); i++)
+    {
+        EV << "    Node #" << i << ":" <<endl;
+        cModule *mod = topo.getNode(i)->getModule();
+        nodeInfo[i].nedTypeName = std::string(mod->getNedTypeName()); //returns the ned type name
+        nodeInfo[i].fullName = std::string(mod->getFullName()); //getFullName() or getName() returns the name assigned in the topology (such as host1, switch1, etc...)
+        EV << "      Ned type: " << nodeInfo[i].nedTypeName << "; Name: " << nodeInfo[i].fullName <<endl;
+
+        //isWSN
+        if (nodeInfo[i].nedTypeName.find("RPLRouter") != std::string::npos) //such as: RPLRouter, ...
+        {
+            nodeInfo[i].isWSN = true;
+            //Add element to WSNInfo vector
+            WSNInfo newWSN;
+            //IInterfaceTable *ift = IPvXAddressResolver().interfaceTableOf(mod);
+            IInterfaceTable *ift = L3AddressResolver().interfaceTableOf(mod);
+
+            int nInterfaces = ift->getNumInterfaces();
+            if(nInterfaces > 2) //If host has more than 2 interfaces...
+                error("The host has more than 2 interfaces (one connected and loopback) and that's still not implemented!");
+            for (unsigned int k=0; k<nInterfaces; k++)
+            {
+                InterfaceEntry *ie = ift->getInterface(k);
+                //We add only the info about the entry which is not the loopback
+                if (!ie->isLoopback())
+                {
+                    newWSN.fullName = nodeInfo[i].fullName;
+                    newWSN.ipAddress = ie->ipv6Data()->getLinkLocalAddress();
+                    newWSN.macAddress = ie->getMacAddress();
+                    //If all WSN host have not RPL, an error will occur in next line
+                    //newWSN.pRPLRouting = check_and_cast<RPLRouting *>(mod->getSubmodule("rpl"));
+                    newWSN.moduleIndex = mod->getIndex();
+                    EV << "        " << newWSN.fullName << "-> IP: " << newWSN.ipAddress << "; MAC: " << newWSN.macAddress << "; Module Index: " << newWSN.moduleIndex << "; Vector index: " << i <<endl;
+                    nWSN++;
+                }
+            }
+
+            wSNInfo.push_back(newWSN);
+        }
+    }
+
+    unsigned int n = wSNInfo.size();
+    EV << "  and " << n << "(active)/" << nWSN << "(total) of those nodes are WSN node" << endl;
+
+    EV << "<-managerRPL::extractTopology()" << endl;
+}
+
+
+void managerRPL::handleMessage(cMessage *msg)
+{
+    EV << "->managerRPL::handleMessage()" << endl;
+
+    throw cRuntimeError("This module doesn't handle message!");
+
+    EV << "<-managerRPL::handleMessage()" << endl;
+}
+
+int managerRPL::getIndexFromAddress(IPv6Address address)
+{
+    EV << "->managerRPL::getIndexFromAddress()" << endl;
+
+    Enter_Method("managerRPL::getIndexFromAddress()");
+    EV << "Vector size is " << wSNInfo.size() << endl;
+
+    for (int i=0; i<wSNInfo.size(); i++){
+        if (wSNInfo.at(i).ipAddress == address)
+            return i;
+    }
+
+    //return -1;
+    throw cRuntimeError("managerRPL::getIndexFromAddress(): Address not found!");
+
+
+    EV << "<-managerRPL::getIndexFromAddress()" << endl;
+}
+
+IPv6Address managerRPL::getAddressFromIndex(int index)
+{
+    EV << "->managerRPL::getAddressFromIndex()" << endl;
+
+    Enter_Method("managerRPL::getAddressFromIndex()");
+
+
+    //if ((index >= 0) && (index < wSNInfo.size()))
+        return wSNInfo.at(index).ipAddress;
+
+    //return IPv6Address::UNSPECIFIED_ADDRESS;
+
+    EV << "<-managerRPL::getAddressFromIndex()" << endl;
+}
+
+std::string managerRPL::getNameFromAddress(IPv6Address address)
+{
+    EV << "->managerRPL::getNameFromAddress()" << endl;
+
+    Enter_Method("managerRPL::getNameFromAddress()");
+
+    for (int i=0; i<wSNInfo.size(); i++){
+        if (wSNInfo.at(i).ipAddress == address)
+            return wSNInfo.at(i).fullName;
+    }
+
+    return "Wrong Name";
+
+    EV << "<-managerRPL::getNameFromAddress()" << endl;
+}
+
+std::string managerRPL::getNameFromIndex(int index)
+{
+    EV << "->managerRPL::getNameFromIndex()" << endl;
+
+    Enter_Method("managerRPL::getNameFromIndex()");
+
+
+    //if ((i > 0) && (i < wSNInfo.size()))
+        return wSNInfo.at(index).fullName;
+
+    //return "Wrong Index";
+    EV << "<-managerRPL::getNameFromIndex()" << endl;
+
+}
+
+void managerRPL::finish()
+{
+    EV << "->managerRPL::finish()" << endl;
+
+    std::string topoAddressInfo;
+    FILE *destfp;
+    if((destfp=fopen("RPLNodeAddresses.txt","w"))!=nullptr)
+    {
+        for (int i=0; i<wSNInfo.size(); i++){
+            topoAddressInfo = "\t" + wSNInfo.at(i).fullName + "\t" + wSNInfo.at(i).ipAddress.str() + wSNInfo.at(i).macAddress.str() + "\n";
+            fprintf(destfp, "%c%S", i, topoAddressInfo.c_str());
+        }
+    }
+    fclose(destfp);
+
+    EV << "<-managerRPL::finish()" << endl;
+}
+
+
+} // nemespace rpl
