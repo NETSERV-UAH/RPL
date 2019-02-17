467bd0db3ee27f4642ded9f317cca6b9724fd906
Hedayat Hosseini <h.hosseini@aut.ac.ir>
2019-02-17 14:44:30 +0330

    Removing unessential files such as compiled files

--- Contiki-ng_4_2/rpl-non-storing/sky-testscript/code/build/sky/obj/heapmem.d
+++
@@ -1,4 +0,0 @@
-build/sky///obj/heapmem.o: ../../../../os/lib/heapmem.c project-conf.h \
- ../../../../os/lib/heapmem.h
-../../../../os/lib/heapmem.c project-conf.h :
- ../../../../os/lib/heapmem.h :
